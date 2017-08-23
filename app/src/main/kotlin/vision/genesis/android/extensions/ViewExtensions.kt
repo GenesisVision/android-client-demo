2737efd9e09f2bbf3ad709721fea4d370cd07729
arsich <mail@arsich.ru>
2017-08-23 09:13:03 +0300

    Add app icon and splash screen

--- app/src/main/kotlin/vision/genesis/android/extensions/ViewExtensions.kt
+++ app/src/main/kotlin/vision/genesis/android/extensions/ViewExtensions.kt
@@ -4,4 +4,4 @@ import android.widget.ImageView
 import com.bumptech.glide.Glide
 import vision.genesis.android.R
 
-fun ImageView.loadUrl(url: String) = Glide.with(context.applicationContext).load(url).error(R.mipmap.ic_launcher).into(this)
+fun ImageView.loadUrl(url: String) = Glide.with(context.applicationContext).load(url).error(R.mipmap.logo).into(this)
